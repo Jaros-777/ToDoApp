e96a1d0d7a492c3e3536ee3a17f3ffd3fd9a24a8
Jaros-777 <filip.jarocki@wp.pl>
2024-09-10 20:45:05 +0200

    Basic front and backend

--- ToDoApp/src/App.jsx
+++ ToDoApp/src/App.jsx
@@ -1,17 +1,54 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
+  const [taskList, setTaskList] = useState([{ id: 1, name: "lol" }]);
+  const [taskName, setTaskName] = useState("");
+
+  function addTask() {
+    if (taskName != "") {
+      const newTask = {
+        id: taskList.length + 1,
+        name: taskName,
+      };
+
+      setTaskList([...taskList, newTask]);
+      setTaskName("");
+    }
+  }
+
+  function deleteTask(taskId) {
+    const updatedTasks = taskList.filter((task) => task.id !== taskId);
+    setTaskList(updatedTasks);
+  }
 
   return (
     <>
-    <div id='main-container'>
-    <p>hej</p>
-
-    </div>
+      <div id="main-container">
+        <h1>To Do App</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Type text"
+            value={taskName}
+            onChange={(e) => setTaskName(e.target.value)}
+          />
+          <button onClick={addTask}>Click</button>
+        </div>
+        <ul>
+          {taskList.map((t) => (
+            <li key={t.id}>
+              <div className="task-container">
+                <p>{t.name}</p>
+                <button onClick={() => deleteTask(t.id)}>Delete</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
