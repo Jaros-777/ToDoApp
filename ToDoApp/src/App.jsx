d68927fee8c23064092721184f2c843d11459d1f
Jaros-777 <filip.jarocki@wp.pl>
2024-09-10 19:56:38 +0200

    Create basics

---
+++ ToDoApp/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div id='main-container'>
+    <p>hej</p>
+
+    </div>
+    </>
+  )
+}
+
+export default App
