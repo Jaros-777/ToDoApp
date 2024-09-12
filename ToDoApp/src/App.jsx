00b8cef82f3f554b853351ac8764890304bb64ec
Jaros-777 <filip.jarocki@wp.pl>
2024-09-12 18:47:30 +0200

    Add Enter to submit

--- ToDoApp/src/App.jsx
+++ ToDoApp/src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import { useEffect } from "react";
 
 import "./App.css";
 
 function App() {
-  const [taskList, setTaskList] = useState([{ id: 1, name: "First Task" , completed: false}]);
+  const [taskList, setTaskList] = useState([{ id: 0, name: "First Task" , completed: false}]);
   const [taskName, setTaskName] = useState("");
+  let nextCurrentId = uuidv4();
+
 
 
   function addTask() {
     if (taskName != "") {
       const newTask = {
-        id: taskList.length + 1,
+        id: nextCurrentId,
         name: taskName,
         completed: false,
       };
@@ -31,12 +35,19 @@ function App() {
 
   }
 
+  
+
   return (
     <>
       <div id="main-container">
         <h1>To Do App</h1>
         <div id="input-field" >
           <input
+            onKeyDown={(e) => {
+              if (e.key == "Enter"){
+                addTask();
+              }
+            }}
             type="text"
             placeholder="Type task"
             value={taskName}
@@ -49,7 +60,7 @@ function App() {
             <li key={t.id}>
               <div className="task-container">
                 <input type="checkbox" checked={t.completed} onChange={()=>doneTask(t.id)}/>
-                <p  style={t.completed ? {textDecoration: "line-through", textDecorationColor: "red", textDecorationThickness: 5} : {textDecoration: "none"}} >{t.name}</p>
+                <p  style={t.completed ? {textDecoration: "line-through", textDecorationColor: "black", textDecorationThickness: 1} : {textDecoration: "none"}} >{t.name}</p>
                 <button onClick={() => deleteTask(t.id)}>Delete</button>
               </div>
             </li>
